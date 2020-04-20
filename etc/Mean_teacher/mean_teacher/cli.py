d3dd2a778553cc0734487682f0e731e4d940f2b2
Donghyun Kim <dong32@gmail.com>
2020-04-20 15:16:41 +0900

    baseline init

---
+++ etc/Mean_teacher/mean_teacher/cli.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2018, Curious AI Ltd. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.`
+
+import re
+import argparse
+import logging
+
+from . import architectures, datasets
+
+
+LOG = logging.getLogger('main')
+
+__all__ = ['parse_cmd_args', 'parse_dict_args']
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+
+    parser.add_argument('--exclude-unlabeled', default=False, type=str2bool, metavar='BOOL',
+                        help='exclude unlabeled examples from the training set')
+    parser.add_argument('--arch', '-a', metavar='ARCH', default='resnext152',
+                        choices=architectures.__all__,
+                        help='model architecture: ' +
+                            ' | '.join(architectures.__all__))
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('--labeled-batch-size', default=None, type=int,
+                        metavar='N', help="labeled examples per minibatch (default: no constrain)")
+    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                        metavar='LR', help='max learning rate')
+    parser.add_argument('--initial-lr', default=0.0, type=float,
+                        metavar='LR', help='initial learning rate when using linear rampup')
+    parser.add_argument('--lr-rampup', default=0, type=int, metavar='EPOCHS',
+                        help='length of learning rate rampup in the beginning')
+    parser.add_argument('--lr-rampdown-epochs', default=None, type=int, metavar='EPOCHS',
+                        help='length of learning rate cosine rampdown (>= length of training)')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--nesterov', default=False, type=str2bool,
+                        help='use nesterov momentum', metavar='BOOL')
+    parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)')
+    parser.add_argument('--ema-decay', default=0.999, type=float, metavar='ALPHA',
+                        help='ema variable decay rate (default: 0.999)')
+    parser.add_argument('--consistency', default=100, type=float, metavar='WEIGHT',
+                        help='use consistency loss with given weight (default: None)')
+    parser.add_argument('--consistency-type', default="mse", type=str, metavar='TYPE',
+                        choices=['mse', 'kl'],
+                        help='consistency loss type to use')
+    parser.add_argument('--consistency-rampup', default=5, type=int, metavar='EPOCHS',
+                        help='length of the consistency loss ramp-up')
+    parser.add_argument('--logit-distance-cost', default=-1, type=float, metavar='WEIGHT',
+                        help='let the student model have two outputs and use an MSE loss between the logits with the given weight (default: only have one output)')
+    parser.add_argument('--print-freq', '-p', default=100, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('-e', '--evaluate', type=str2bool,
+                        help='evaluate model on evaluation set')
+    parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                        help='use pre-trained model')
+
+    parser.add_argument('--start_epoch', type=int, default=1, metavar='N', help='number of start epoch (default: 1)')
+    parser.add_argument('--epochs', type=int, default=200, metavar='N', help='number of epochs to train (default: 200)')
+
+    # basic settings
+    parser.add_argument('--name',default='Res18baseMM', type=str, help='output model name')
+    parser.add_argument('--gpu_ids',default='0', type=str,help='gpu_ids: e.g. 0  0,1,2  0,2')
+    parser.add_argument('--batchsize', default=50, type=int, help='batchsize')
+    parser.add_argument('--seed', type=int, default=123, help='random seed')
+
+    # basic hyper-parameters
+    parser.add_argument('--imResize', default=256, type=int, help='')
+    parser.add_argument('--imsize', default=224, type=int, help='')
+    parser.add_argument('--lossXent', type=float, default=1, help='lossWeight for Xent')
+
+    # arguments for logging and backup
+    parser.add_argument('--log_interval', type=int, default=10, metavar='N', help='logging training status')
+    parser.add_argument('--save_epoch', type=int, default=50, help='saving epoch interval')
+
+    # hyper-parameters for mix-match
+    parser.add_argument('--alpha', default=0.75, type=float)
+    parser.add_argument('--lambda-u', default=75, type=float)
+    parser.add_argument('--T', default=0.5, type=float)
+
+    ### DO NOT MODIFY THIS BLOCK ###
+    # arguments for nsml
+    parser.add_argument('--pause', type=int, default=0)
+    parser.add_argument('--mode', type=str, default='train')
+    ################################
+    return parser
+
+
+def parse_commandline_args():
+    return create_parser().parse_args()
+
+
+def parse_dict_args(**kwargs):
+    def to_cmdline_kwarg(key, value):
+        if len(key) == 1:
+            key = "-{}".format(key)
+        else:
+            key = "--{}".format(re.sub(r"_", "-", key))
+        value = str(value)
+        return key, value
+
+    kwargs_pairs = (to_cmdline_kwarg(key, value)
+                    for key, value in kwargs.items())
+    cmdline_args = list(sum(kwargs_pairs, ()))
+
+    LOG.info("Using these command line args: %s", " ".join(cmdline_args))
+
+    return create_parser().parse_args(cmdline_args)
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def str2epochs(v):
+    try:
+        if len(v) == 0:
+            epochs = []
+        else:
+            epochs = [int(string) for string in v.split(",")]
+    except:
+        raise argparse.ArgumentTypeError(
+            'Expected comma-separated list of integers, got "{}"'.format(v))
+    if not all(0 < epoch1 < epoch2 for epoch1, epoch2 in zip(epochs[:-1], epochs[1:])):
+        raise argparse.ArgumentTypeError(
+            'Expected the epochs to be listed in increasing order')
+    return epochs
